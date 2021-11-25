0a1df1986efd3c12cfeb3404c0719884ff78492e
Antonios Pavlakis <antonios.pavlakis@staysafeapp.com>
2021-11-25 22:04:26 +0000

    Parse csv message and trim spaces

--- src/Socket.php
+++ src/Socket.php
@@ -26,11 +26,20 @@ class Socket implements MessageComponentInterface {
     public function onMessage(ConnectionInterface $from, $msg) {
         foreach ($this->clients as $client) {
             if ($from === $client) {
-                $sequence = explode(',', $msg);
+                $sequence = $this->parseMessage($msg);
                 $result = $this->masterMind->play($sequence);
                 $client->send(json_encode($result, JSON_THROW_ON_ERROR));
             }
         }
+    }
+
+    private function parseMessage($msg): array
+    {
+        if (is_array($msg)) {
+            return $msg;
+        }
+
+        return array_map('trim', explode(',', $msg));
     }
 
     public function onClose(ConnectionInterface $conn) {
