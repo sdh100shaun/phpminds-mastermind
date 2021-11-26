a11f11c965fbc40d438212a41be49808609f1d2a
Shaun Hare <shaun@shaunhare.co.uk>
2021-11-26 20:49:42 +0000

    wip

--- public/main.js
+++ public/main.js
@@ -1,10 +1,18 @@
-import {mastermind} from "./messages.js";
+import { store } from "./storage.js";
 
 const url = '127.0.0.1:8085';
 let socket = new WebSocket('ws://' + url);
-let mm = mastermind().init(socket);
-console.log(mm);
 
 socket.onmessage = function (e) {
     alert(e.data)
 }
+
+const sendMessage = (message) => {
+    socket.send(message);
+}
+
+document.getElementById('btn').addEventListener('click', function () {
+    sendMessage("{\"0\":\"red\", \"1\":\"green\", \"2\":\"purple\", \"3\":\"yellow\"}");
+});
+
+init(socket);
